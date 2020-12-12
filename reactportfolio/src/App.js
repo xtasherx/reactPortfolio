914f27ae581554b152d6d6ae13bf28fd3d328070
Tasha <xtasherx@gmail.com>
2020-12-12 12:10:05 -0600

    attempting hashrouter with class

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+// import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
+  
+import React, { Component } from 'react';
 import "cirrus-ui";
 import Home from "./Home";
 import ContactMe from "./ContactMe";
@@ -9,28 +12,47 @@ import About from "./About";
 
 
 
-function App() {
-  return (
-    <Router basename="/">
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route  path = "/about">
-          <About />
-        </Route>
-        <Route  path="/work">
-          <Work />
-        </Route>
-        <Route  path="/contactme">
-          <ContactMe />
-        </Route>
-      </Switch>
-    </div>
-  </Router>
-  )
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename="/">
+        <div>
+        <Header />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contactme" component={ContactMe} />
+        <Route path="/work" component={Work} />
+
+     
+        </div>
+      </HashRouter >
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <Router basename="/">
+//     <div>
+//       <Header />
+//       <Switch>
+//         <Route exact path="/">
+//           <Home />
+//         </Route>
+//         <Route path = "/about">
+//           <About />
+//         </Route>
+//         <Route path="/work">
+//           <Work />
+//         </Route>
+//         <Route path="/contactme">
+//           <ContactMe />
+//         </Route>
+//       </Switch>
+//     </div>
+//   </Router>
+//   )
+// }
 
 export default App;
--- reactportfolio/src/Header.js
+++ reactportfolio/src/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
             </div>
             <div className="header-nav">
                 <div className="nav-center">
-                <a className="m-1 text-primary uppercase hover-grow"  href="/home">Home</a>
+                <a className="m-1 text-primary uppercase hover-grow"  href="/">Home</a>
                 <a className="m-1 text-primary uppercase hover-grow"  href="/#/about">About Me</a>
                 <a className="m-1 text-primary uppercase hover-grow"  href="/#/contactme">Contact Me</a>
                 <a className="m-1 text-primary uppercase hover-grow"  href="/#/work">My Work</a>
